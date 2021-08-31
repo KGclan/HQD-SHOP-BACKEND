477f724c068241a71b738820e857f89f5c01fc8f
KGclan <kirill---gorbenko@mail.ru>
2021-08-31 09:21:00 +0300

    add cors

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,18 @@
+from django.db.models import fields
 from rest_framework import serializers
 from .models import Tastes, Product
 
 
+class TastesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tastes
+        fields = ["name"]
+
 class ProductSerializer(serializers.ModelSerializer):
+
+    tastes = TastesSerializer(many=True)
+
     class Meta:
         model = Product
         fields = ["id","name","img","description","tastes"]
--- hqdShopApi/settings.py
+++ hqdShopApi/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'api','media') 
 MEDIA_URL = '/media/'
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000"
+]
